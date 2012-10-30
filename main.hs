30dc8f149a51529fc256eb2aef88727bfd16f0af
bartsmolders <bart_smolders557@hotmail.com>
2012-10-30 14:10:37 +0100

    basic land parser

--- main.hs
+++ main.hs
@@ -1,19 +1,34 @@
 module Main where
 
+import Data.Char
 import TooSimpleParseLib
 
-func :: Parser Char String
-func = (many $ pSatisfy ( /=')' ))
+pStatement :: String
+pStatement = show pCountry
 
-func1 :: Parser Char String
-func1 = (many $ pSatisfy ( /='}' ))
-{-
-pMany :: f -> [Char] -> (f -> [Char])
-pMany f lis@(x:xs) | many pSatisfy (/= ".") = pMany f list
-				   | f list-}
+pCountry :: Parser Char String
+pCountry = (\_ c1 c2 -> c1 ++ c2) <$> overig
+								 <* pToken " "
+								 <*> pCapital
+								 <*> pRest
 
+pCapital :: Parser Char String
+pCapital = (many $ pSatisfy (`elem` ['A'..'Z']))
 
+pRest :: Parser Char String
+pRest = (many $ pSatisfy ( /=' ' ))
 
+overig :: Parser Char String
+overig = (many $ pSatisfy (`elem` ['a'..'z']))
+
+keywords :: [String]
+keywords = ["borders", "border"]
+
+pKeywords :: String -> String
+pKeywords s (x:xs) = if s `elem` keyword then s else pKeywords s xs
+pKeywords s [] = _
+
+--Testing
 myParse :: Parser Char (String,String)
 myParse = (\_ ex bdy -> (ex,bdy) ) <$> pToken "if("
 								 <*> func
@@ -21,3 +36,12 @@ myParse = (\_ ex bdy -> (ex,bdy) ) <$> pToken "if("
 								 <*> func1
 								 <* pSym '}'
 
+func :: Parser Char String
+func = (many $ pSatisfy ( /=')' ))
+
+func1 :: Parser Char String
+func1 = (many $ pSatisfy ( /='}' ))
+
+{-}
+pMany :: Parser a a -> Parser a a
+pMany p = -}
